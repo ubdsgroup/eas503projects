deffeae803cfe665cb189c6419d6af38c224faad
pjoshi5 <43094865+pjoshi5@users.noreply.github.com>
2018-12-14 21:29:07 -0500

    Add files via upload

---
+++ Project2018/Group 12/Rossmann.sql
@@ -0,0 +1,89 @@
+show databases;
+DROP DATABASE IF EXISTS Rossmann_Store;
+CREATE DATABASE IF NOT EXISTS Rossmann_Store;
+USE Rossmann_Store;
+show tables;
+
+CREATE TABLE IF NOT EXISTS Store(
+  Store INT,
+  StoreType varchar(64),
+  Assortment varchar(64),
+  CompetitionDistance varchar(64),
+  CompetitionOpenSinceMonth varchar(64),
+  CompetitionOpenSinceYear varchar(64),
+  Promo2 INT,
+  Promo2SinceWeek varchar(64),
+  Promo2SinceYear varchar(64),
+  PromoInterval char(64),
+  PRIMARY KEY(Store)
+  );
+LOAD DATA LOCAL INFILE 'store.csv' INTO TABLE  Store FIELDS TERMINATED BY ','  ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+
+select * from Store;
+
+CREATE TABLE Sales(
+  Store INT,
+  Date VARCHAR(64),
+  DayOfWeek INT,
+  Sales INT,
+  Customers INT,
+  Open INT,
+  Promo INT,
+  StateHoliday varchar(64),
+  SchoolHoliday INT,
+  PRIMARY KEY(Store,Date),
+  FOREIGN KEY (Store)  REFERENCES Store (Store)
+  );
+LOAD DATA LOCAL INFILE 'sales.csv' INTO TABLE  Sales FIELDS TERMINATED BY ',' ESCAPED BY '"' IGNORE 1 LINES;
+UPDATE Sales SET Date = STR_TO_DATE(Date, '%c/%e/%Y');
+ALTER TABLE Sales MODIFY Date date;
+
+CREATE TABLE IF NOT EXISTS Rossmann_Store_DataWarehouse(
+  Store int,
+  Date VARCHAR(10),
+  DayOfWeek int,
+  Sales int,
+  Customers int,
+  Open int,
+  Promo int,
+  StateHoliday char(1),
+  SchoolHoliday int,
+  StoreType char(1),
+  Assortment char(1),
+  CompetitionDistance int,
+  CompetitionOpenSinceMonth int,
+  CompetitionOpenSinceYear int,
+  Promo2 int,
+  Promo2SinceWeek int,
+  Promo2SinceYear int,
+  PromoInterval char(64),
+  PRIMARY KEY(Store,Date)
+  );
+  LOAD DATA LOCAL INFILE 'store_data_DW.csv' INTO TABLE  Store FIELDS TERMINATED BY ','  ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
+  UPDATE Rossmann_Store_DataWarehouse SET Date = STR_TO_DATE(Date, '%c/%e/%Y');
+  ALTER TABLE Rossmann_Store_DataWarehouse MODIFY Date date;
+
+
+CREATE TABLE IF NOT EXISTS Rossmann_Store_DataWarehouse(
+  Store int,
+  Date VARCHAR(10),
+  DayOfWeek int,
+  Sales int,
+  Customers int,
+  Open int,
+  Promo int,
+  StateHoliday char(1),
+  SchoolHoliday int,
+  StoreType char(1),
+  Assortment char(1),
+  CompetitionDistance int,
+  CompetitionOpenSinceMonth int,
+  CompetitionOpenSinceYear int,
+  Promo2 int,
+  Promo2SinceWeek int,
+  Promo2SinceYear int,
+  PromoInterval char(64),
+  PRIMARY KEY(Store,Date)
+  );
+  UPDATE Sales SET Date = STR_TO_DATE(Date, '%c/%e/%Y');
+ALTER TABLE Sales MODIFY Date date;
